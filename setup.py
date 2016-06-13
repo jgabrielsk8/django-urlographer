63a84aa11cd4289c6b94897b93cb67d6f9299460
Waldecir Santos <waldecir@gmail.com>
2016-06-13 18:24:25 -0300

    bump version.

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ from setuptools import setup
 
 setup(
     name='django-urlographer',
-    version='0.9.8',
+    version='0.9.9',
     author='Josh Mize',
     author_email='jmize@consumeraffairs.com',
     description='URL mapper for django',
