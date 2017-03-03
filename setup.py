7c4c8ec2786a81348af085c789b9c2b606f8583a
J.V. Zammit <jv@jv-mbpro.local>
2017-03-03 14:49:50 +0100

    SEO-509 Updates minor version number.

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='django-urlographer',
-    version='0.10.6',
+    version='0.10.7',
     author='Josh Mize',
     author_email='jmize@consumeraffairs.com',
     description='URL mapper for django',
